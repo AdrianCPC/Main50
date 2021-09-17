70225083ea4b08108c2ea088f5fbfee2ec665f85
AdrianPC <adrianchooo18@gmail.com>
2021-09-16 22:40:49 -0500

    correcion de algunos errores en js

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 //Declaration variables from document
-let starts = document.getElementById('starts');
-let moon = document.getElementById('moon');
-let mountains_behind = document.getElementById('mountains_behind');
-let text = document.getElementById('text');
-let btn = document.getElementById('btn');
-let mountains_front = document.getElementById('mountains_front');
-let header = document.querySelector('header');
+const starts = document.getElementById('starts');
+const moon = document.getElementById('moon');
+const mountains_behind = document.getElementById('mountains_behind');
+const text = document.getElementById('text');
+const btn = document.getElementById('btn');
+const mountains_front = document.getElementById('mountains_front');
+const header = document.querySelector('header');
 
 //Events listener
+const value = 0;
 window.addEventListener('scroll', function () {
-    let value = window.scrollY;
-    starts.style.left = value * 0.25 + 'px';
+    const value = window.scrollY;
+    console.log(window.scrollY);
     moon.style.top = value * 1.05 + 'px';
+    // starts.style.left = value * 0.25 + 'px';    
     mountains_behind.style.top = value * 0.5 + 'px';
     mountains_front.style.top = value * 0 + 'px';
     text.style.marginRight = value * 4 + 'px';
-    text.style.marginTop = value * 1.5 + 'px';
-    btn.style.marginTop = value * 1.5 + 'px';
+    // text.style.marginTop = value * 1.5 + 'px';
+    // btn.style.marginTop = value * 1.5 + 'px';
     header.style.Top = value * 0.5 + 'px';
 })
