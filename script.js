a8743746e6cd757e7a9134c99a4e451d16e81612
AdrianPC <adrianchooo18@gmail.com>
2021-09-17 10:18:08 -0500

    search and fonts

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ const header = document.querySelector('header');
 const value = 0;
 window.addEventListener('scroll', function () {
     const value = window.scrollY;
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
     moon.style.top = value * 1.05 + 'px';
     // starts.style.left = value * 0.25 + 'px';    
     mountains_behind.style.top = value * 0.5 + 'px';
     mountains_front.style.top = value * 0 + 'px';
-    text.style.marginRight = value * 4 + 'px';
-    // text.style.marginTop = value * 1.5 + 'px';
+    text.style.marginRight = value * 3 + 'px';
+    text.style.marginTop = value * 1.2 + 'px';
     // btn.style.marginTop = value * 1.5 + 'px';
     header.style.Top = value * 0.5 + 'px';
 })
